ed6f996c9c12bc3d79e6a965574a25e531877e40
Pavel Yudkevich <29350384+metalowner@users.noreply.github.com>
2018-02-06 17:11:19 +0200

    Initial canvas properties
    added some inputs, designs, etc...

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 var Practices = [
 	{
+		title: 'Life Story',
+		description: 'Write your life story starting early childhood focusing on events,' + 
+		' relationships, achievements and feelings. This practice will help this app or people understand you better.'
+	},
+	{
 		title: 'Best day of your life analysis',
 		description: 'Write the first thing that' +
 		 ' comes to your mind as you try to remember' +
