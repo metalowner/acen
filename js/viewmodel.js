3f6d1e2f31b6387208f942ae7fb513890718481c
Pavel Yudkevich <29350384+metalowner@users.noreply.github.com>
2018-02-23 13:47:33 +0200

    update 23/02/2018

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,15 +1,21 @@
 function AppViewModel() {
-	this.data = ko.observableArray(Practices);
-	this.userName = 'Pavel Yudkevich';
-	this.displayedTitle = ko.observable("");
-	this.displayedDescription = ko.observable("");
-	this.startLearning = function() {
-		console.log(this.data()[0]);
-		this.title = this.data()[0].title;
-		this.description = this.data()[0].description;
-		this.displayedTitle(this.title);
-		this.displayedDescription(this.description);
-	}
+	var self = this;
+	this.displayedMessage = ko.observable("");
+	this.answer = ko.observable("");
+	this.displayedMessage(BotMessages[0]);
+	this.displayedQuestion = ko.observable("");
+	this.displayedQuestion(Questions[0].questionDescription);
+	this.questionHeader = ko.observable("");
+	this.questionHeader(Questions[0].questionTitle);
+	this.submitFunction = function() {};
+	this.nextQuestion = function() {
+		this.displayedQuestion(Questions[1].questionDescription);
+		this.questionHeader(Questions[1].questionTitle);
+	};
+	this.previousQuestion = function() {
+		this.displayedQuestion(Questions[0].questionDescription);
+		this.questionHeader(Questions[0].questionTitle);
+	};
 }
 
 ko.applyBindings(new AppViewModel());
